4aa07a9063b2dc1e68f1727a882e42821a6dfdfa
Dimawete <mawetedinho490@gmail.com>
2025-04-23 09:53:12 -0700

    Add files via upload

---
+++ week1/src/Concatenation.java
@@ -0,0 +1,13 @@
+public class Concatenation {
+
+    public static void main(String[]args) {
+        String color = "red";
+        System.out.println("My favor color is " +color);
+
+        int money = 2_000_000;
+        System.out.println("I hit the jacket and won $" +money+ " dollars!");
+        System.out.println(color+" " +money);
+        System.out.println("This will be my system regarding my favorite color which is " +color+ " and will let you know that I won $ " +money+ " dollar");
+
+    }
+}
---
+++ week1/src/Myvariable1.java
@@ -0,0 +1,36 @@
+public class Myvariable1 {
+    public static void main(String[] args) {
+    String name ="Mambuanda"; //after a statement, do not forget the semi colon!!!
+          int hour = 40; //we are assigning 40 to the hour variable
+          // please note, when we have a string value, it is nested in ""
+          //when we are referring to numbers/integers, no quotes!!!
+          System.out.println(name);
+          System.out.println(hour);
+
+        String building, tree, lake, ocean;
+        tree = "Redwood";
+        building = "Empire State";
+        lake = "Lake Superior";
+        ocean = "Pacific Ocean";
+
+//        String building = "Empire State", tree ="Maple", lake ="Lake Superior", ocean ="Pacific Ocean";
+
+        int week, decades, months, years;
+        week = 4;
+        decades = 10;
+        months = 3;
+        years = 5;
+
+//        int weeks =4, decades =10, months =3, years =5;
+
+
+        System.out.println(tree);
+        System.out.println(building);
+        System.out.println(lake);
+        System.out.println(ocean);
+        System.out.println(week);
+        System.out.println(decades);
+        System.out.println(years);
+
+    }
+}
